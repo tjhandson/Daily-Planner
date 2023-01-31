f9bf7adeb97b8c6befe3f7d73c2a39e574788a33
tjhandson <73173000+tjhandson@users.noreply.github.com>
2023-01-31 21:51:35 +0000

    adding colour conditional formatting for time slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,22 +11,22 @@ let hour = 9;
 function timeSlotGeneration() {
 
     for (let i = 0; i < 9; i++) {
-        let time = moment(hour, "hh").format("hA");
+        let time = moment(hour, "hh").format("H A");
 
         //Row for time slot
-        const timeSlot = $("<div>").addClass("row mx-auto");
+        const timeSlot = $("<div>").addClass("row time-block mx-auto");
 
         // Section for Hour
         const hourText = $("<h5>").addClass("hourtext text-right").text(time)
         const hourEl = $("<div>").addClass("hour col-1").append(hourText);
 
         // User input area for text entry
-        const taskEntry = $("<textarea>").addClass("UserInput overflow-auto text-dark col-8").text("").attr("id", time);
-
+        const taskEntry = $("<textarea>").addClass(" overflow-auto text-dark col-8").text("").attr("id", time);
+        colourCodeTimeBackground(taskEntry);
 
         // Save Button block at end of row 
         const saveIcon = $("<span>").addClass("fa fa-save");
-        const saveButton = $("<button>").addClass("btn saveBtn time-block col-1").append(saveIcon);
+        const saveButton = $("<button>").addClass("btn saveBtn col-1").append(saveIcon);
 
         // Appending hour, text entry and button to time slot
         timeSlot.append(
@@ -44,6 +44,19 @@ function timeSlotGeneration() {
     }
 }
 // Task 3: Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+function colourCodeTimeBackground(taskEntry) {
+    let currentTime = parseInt(moment().format("H A")); // Gets current hour in 24 hour format
+
+    if (hour < currentTime) {
+        taskEntry.addClass("past");
+    }
+    else if (hour > currentTime) {
+        taskEntry.addClass("future");
+    }
+    else if (hour === currentTime) {
+        taskEntry.addClass("present");
+    }
+}
 
 // Task 4: Allow a user to enter an event when they click a timeblock
 
